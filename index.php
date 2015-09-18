413c70303369d7570a0dd1ec63dde1bfa5a5edf0
Montana <montanawong@gmail.com>
2015-09-18 11:37:43 -0400

    project page under construction

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
         </p>
         <div class="features">
             <div class="feature">
-                <a href="projects.php">
+                <a href="under_construction.php">
                     <i class="fa fa-diamond"></i>
                     <h3>Software Projets</h3>
                     <p>A handful of projects from a variety of applications</p>
---
+++ projects.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: montanawong
+ * Date: 9/1/15
+ * Time: 15:51
+ */
+include("includes/header.php");
+?>
+
+<!-- one-->
+<section id="one" class="wrapper special" style="padding-top: .5em;">
+    <div class="inner">
+        <header class="major" style="margin-bottom: 0em;">
+            <h2>Projects</h2>
+        </header>
+
+    </div>
+</section>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
